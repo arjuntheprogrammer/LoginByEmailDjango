313e4028cd37feeda54b9af3a7b364071d86dbb3
arjun gupta <saimzn4241@gmail.com>
2018-10-23 00:39:05 +0530

    app

--- LoginByEmail/settings.py
+++ LoginByEmail/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-AUTH_USER_MODEL = 'adminComplaint.MyUser'
+# AUTH_USER_MODEL = 'adminComplaint.MyUser'
+AUTH_USER_MODEL = 'app.User'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
